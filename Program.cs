1c4f40e9e426b9bf8e948cef0c8a52257cd32b9e
Tobias Allweier <toallwei@microsoft.com>
2024-12-18 11:17:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEnableNetworkLogging
+{
+    internal class Program
+    {        
+        private static HttpClient sharedClient = new HttpClient()
+        {
+            BaseAddress = new Uri("https://www.bing.com"),
+        };
+
+        static async Task Main(string[] args)
+        {
+            var response = await sharedClient.GetAsync("/");
+        }
+
+
+    }
+}
